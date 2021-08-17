d3ad7069315ac66a6382d85b9b0e5e4f6265bb65
Roneilla Bumanlag <roneillabumanlag@gmail.com>
2021-08-17 14:07:36 -0400

    Update index.js

--- Template/index.js
+++ Template/index.js
@@ -1,33 +1,9 @@
 // initialize your global variables here
-
-let testVideo;
-
 const onxrloaded = () => {
-	const initXrScene = ({ scene, camera }) => {
-		testVideo = new Video({
-			name: "test",
-			src: "assets/test.mp4",
-			scene: scene,
-			greenScreen: false,
-			orientation: "landscape",
-			scale: 4,
-		});
+	const initXrScene = ({ scene, camera, renderer }) => {
+		renderer.shadowMap.enabled = true;
 
-		camera.position.set(0, 3, 0);
-	};
-
-	const showImageFrame = ({ detail }) => {
-		testVideo.showVideo({
-			posX: detail.position.x,
-			posZ: detail.position.z,
-			rotX: 0,
-			rotY: 0,
-			rotZ: 0,
-		});
-	};
-
-	const hideImageFrame = ({ detail }) => {
-		testVideo.hideVideo();
+		camera.position.set(0, 2, 0);
 	};
 
 	// if you want to disable word tracking, uncomment below:
@@ -41,7 +17,7 @@ const onxrloaded = () => {
 		XRExtras.AlmostThere.pipelineModule(), // Detects unsupported browsers and gives hints.
 		XRExtras.FullWindowCanvas.pipelineModule(), // Modifies the canvas to fill the window.
 		XRExtras.RuntimeError.pipelineModule(), // Shows an error image on runtime error.
-		XRExtras.Loading.pipelineModule(),           // Manages the loading screen on startup.
+		XRExtras.Loading.pipelineModule(), // Manages the loading screen on startup.
 	]);
 
 	XR8.addCameraPipelineModule({
@@ -52,12 +28,17 @@ const onxrloaded = () => {
 		// onStart is called once when the camera feed begins. In this case, we need to wait for the
 		// XR8.Threejs scene to be ready before we can access it to add content. It was created in
 		// XR8.Threejs.pipelineModule()'s onStart method.
-		onStart: ({ canvasWidth, canvasHeight }) => {
+		onStart: ({ canvas }) => {
 			// Get the 3js sceen from xr3js.
-			const { scene, camera } = XR8.Threejs.xrScene();
+			const { scene, camera, renderer } = XR8.Threejs.xrScene();
 
 			// Add some objects to the scene and set the starting camera position.
-			initXrScene({ scene, camera });
+			initXrScene({ scene, camera, renderer });
+
+			// prevent scroll/pinch gestures on canvas
+			canvas.addEventListener("touchmove", (event) => {
+				event.preventDefault();
+			});
 
 			// Sync the xr controller's 6DoF position and camera paremeters with our scene.
 			XR8.XrController.updateCameraProjectionMatrix({
@@ -69,11 +50,6 @@ const onxrloaded = () => {
 		// onUpdate is called once per camera loop prior to render. Any 3js geometry scene
 		// would typically happen here.
 		onUpdate: () => {},
-		listeners: [
-			{ event: "reality.imagefound", process: showImageFrame },
-			{ event: "reality.imageupdated", process: showImageFrame },
-			{ event: "reality.imagelost", process: hideImageFrame },
-		],
 	});
 
 	const canvas = document.getElementById("camerafeed");
@@ -82,7 +58,7 @@ const onxrloaded = () => {
 	canvas.addEventListener(
 		"touchstart",
 		(e) => {
-			e.touches.length == 2 && XR8.XrController.recenter();
+			e.touches.length == 1 && XR8.XrController.recenter();
 		},
 		true
 	);
